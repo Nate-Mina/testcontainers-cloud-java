f4ba4a7e2b4917ea757261b98c98aafabe8550d7
Nathaniel Mina <nate@purecomp.net>
2025-03-14 03:23:44 -0400

    Initialize fix_mvn_cmd

---
+++ src/test/java/cloud/testcontainers/example/TestcontainersCloudFirstTest.java
@@ -0,0 +1,37 @@
+package cloud.testcontainers.example;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Info;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(TccTestWatcher.class)
+public class TestcontainersCloudFirstTest {
+
+    @Test
+    public void canRunContainers() {
+        try (
+                GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("redis:6.0.5"))
+                        .withExposedPorts(6379)
+        ) {
+            container.start();
+        }
+    }
+
+    @Test
+    public void testcontainersCloudDockerEngine() {
+        DockerClient client = DockerClientFactory.instance().client();
+        Info dockerInfo = client.infoCmd().exec();
+
+        assertThat(dockerInfo.getServerVersion())
+                .as("Docker Client has to be connected to Testcontainers Cloud")
+                .contains("testcontainerscloud");
+
+        System.out.println(PrettyStrings.logo);
+    }
+}
